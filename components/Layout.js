9d33ee02c8a9b06c42e3ed1edb4a69e74661be5f
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-20 04:40:25 +0100

    manifest to layout

--- components/Layout.js
+++ components/Layout.js
@@ -8,6 +8,7 @@ export default function Layout({ children }) {
         <div id={styles.layout}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <link rel="manifest" href="/manifest.json" />
             </Head>
             
             { children }
