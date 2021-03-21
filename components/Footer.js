da514babf011ca26071604ac95d451911b7da9d0
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-21 18:37:25 +0100

    contact

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,10 @@ import styles from '../styles/Footer.module.css'
 export default function Footer() {
     return(
         <div id={styles.footer}>
-            <p id={styles.footertext}></p>
+            <p id={styles.footertext}>OSITA EZEIGBO</p>
+            <p id={styles.footertitle}>Web Developer / Front-End Developer</p>
+            <hr id={styles.footerhr}/>
+            <p id={styles.footeremail}><a href="mailto:osiclin@yahoo.com"><u>osiclin@yahoo.com</u></a></p>
         </div>
     )
 }
