5656f940a852ddffcbc31de4882b0eb88da87b5e
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-22 21:43:51 +0100

    project

--- components/Projectcard.js
+++ components/Projectcard.js
@@ -2,18 +2,18 @@ import styles from '../styles/Projectcard.module.css'
 import Image from 'next/image'
 
 export default function Projectcard(props) {
-    
 
     return( 
         <div>
             <div className={styles.projectcard}>
                 <div className={styles.imgwrapper}>
-                <Image src={props.src} className={styles.cardimg} width={props.width} height={300} preload="true" alt="project" />
+                <a href={props.href}>
+                    <Image src={props.src} className={styles.cardimg} width={props.width} height={300} preload="true" alt="project" />
+                </a>
                 </div>
                 <div className={styles.projectdetails}>
                     <h3 className={styles.title}>{props.title}</h3>
                     <p className={styles.para}>{props.details}</p>
-                    <a href={props.href}><input className={styles.button} type="button" value="Visit" /></a>
                 </div>
             </div>
         </div>
