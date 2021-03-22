695297ad74a5f98504eb23a4cd1fa07423f2b0ac
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-22 16:39:49 +0100

    image height

--- pages/projects.js
+++ pages/projects.js
@@ -22,8 +22,8 @@ export default function Projects() {
                         <Projectcard src='/images/md.jpg' width="400px" title="- myTodo" details="A Todo List App. Helps you keep track of events." href="https://mytodoapp-osiclin.vercel.app/" />
                         <Projectcard src='/images/mad.jpg' width="400px" title="- markup" details="MarkUp is a free figma design i stumbled upon on the internet, loved it & decided to develope it." href="https://osiclin.github.io/markup" />
                         <Projectcard src='/images/od.jpg' width="400px" title="- oneburner" details="This is a design i stumbled upon on the internet. I redesigned it, developed it, & made it look better ." href="https://osiclin.github.io/oneburner" />
+                        <Projectcard src='/images/epm.jpg' width="160px" title="- elpadrino" details="(Mobile Version Only). El Padrino is a mexican food store Web App designed and developed by me." href="https://osiclin.github.io/elpadrino" />
                         <Projectcard src='/images/ud.jpg' width="400px" title="- unscramble words" details="In this word game questions are asked at random and players are given a clue to help answer each question." href="https://osiclin.github.io/unscramblewords" />
-                        <Projectcard src='/images/epm.jpg' width="180px" title="- elpadrino" details="(Mobile Version Only). El Padrino is a mexican food store Web App designed and developed by me." href="https://osiclin.github.io/elpadrino" />
                         {/* <Projectcard src='/images/.jpg' title="- lyrics" details="(Still in development). Search lyrics of your favourite song & sing along." href="https://osiclin.github.io/lyrics" /> */}
                     </div>
                     
