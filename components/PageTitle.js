bed68c13469671b00cbb7b232229524ab9e14012
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-26 13:53:42 +0100

    projectcard width

--- components/PageTitle.js
+++ components/PageTitle.js
@@ -1,6 +1,9 @@
+import { useEffect, useRef } from 'react'
 import styles from '../styles/PageTitle.module.css'
+import $ from 'jquery'
 
 export default function PageTitle(props) {
+
     return(
         <div>
             <div className={styles.title}>
--- components/Projectcard.js
+++ components/Projectcard.js
@@ -1,6 +1,7 @@
 import styles from '../styles/Projectcard.module.css'
 import Image from 'next/image'
 
+
 export default function Projectcard(props) {
 
     return( 
