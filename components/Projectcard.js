5b5c30699321b52e2cd1669c17826a55638c603f
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-03-28 08:04:16 +0100

    links

--- components/Projectcard.js
+++ components/Projectcard.js
@@ -8,12 +8,12 @@ export default function Projectcard(props) {
         <div>
             <div className={styles.projectcard}>
                 <div className={styles.imgwrapper}>
-                    <Image src={props.src} className={styles.cardimg} width={props.width} height={300} preload="true" alt="project" />
+                    <a href={props.href}>
+                        <Image src={props.src} className={styles.cardimg} width={props.width} height={300} preload="true" alt="project" />
+                    </a>
                 </div>
                 <div className={styles.projectdetails}>
-                    <a href={props.href}>
-                        <h3 className={styles.title}>{props.title}</h3>
-                    </a>
+                    <h3 className={styles.title}>{props.title}</h3>
                     <p className={styles.para}>{props.details}</p>
                 </div>
             </div>
