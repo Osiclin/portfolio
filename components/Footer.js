52fa29278c14b25fa7765a24d5c4b54e75cee73f
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-05-17 02:53:34 +0100

    links update

--- components/Footer.js
+++ components/Footer.js
@@ -10,8 +10,8 @@ export default function Footer() {
             <ul id={styles.iconwrapper}>
                 
                 <li><a href="https://github.com/osiclin" target="_blank"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/github-square.svg" /></a></li>
-                <li><a href="https://linkedin.com/in/osita-ezeigbo-295aaab6"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/linkedin-square.svg" /></a></li>
-                <li><a href="https://twitter.com/melengeh"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/twitter-square.svg" /></a></li>
+                <li><a href="https://linkedin.com/in/osita-ezeigbo-295aaab6" target="_blank"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/linkedin-square.svg" /></a></li>
+                <li><a href="https://twitter.com/melengeh" target="_blank"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/twitter-square.svg" /></a></li>
                 <li><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/facebook-square.svg" /></li>
                 <li><a href="tel:+2348033779715"><img className={styles.icons} src="https://raw.githubusercontent.com/encharm/Font-Awesome-SVG-PNG/3cfbcdaff9818c3e2c07d755d556fe1f34d7cf0d/black/svg/phone-square.svg" /></a></li>
                 
