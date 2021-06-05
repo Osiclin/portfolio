106705f1850eb94fc9876fc29f4fc6f199332e0a
Osiclin <67206380+Osiclin@users.noreply.github.com>
2021-06-05 18:36:21 +0100

    metadata

--- components/Layout.js
+++ components/Layout.js
@@ -8,9 +8,13 @@ export default function Layout({ children }) {
         <div id={styles.layout}>
             <Head>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-                <meta name="description" content="Osita Ezeigbo is a frontend developer who builds websites and web applications" />
-                <meta name="keywords" content="osita ezeigbo, frontend developer, web developer" />
-                <meta name="author" content="osita ezeigbo" />
+                <meta name="description" content="Osita Ezeigbo Portfolio" />
+                <meta name="keywords" content="Osita Ezeigbo, Software Engineer, Frontend Developer, Web Developer" />
+                <meta name="author" content="Osita Ezeigbo" />
+                <meta property="og:title" content="Osita Ezeigbo Portfolio" />
+                <meta property="og:description" content="I’m a frontend developer based in Lagos, Nigeria." />
+                <meta property="og:type" content="website" />
+                <meta property="og:site_name" content="Osita Ezeigbo" />
                 <link rel="manifest" href="/manifest.json" />
                 <script src="https://kit.fontawesome.com/9baa8c6e06.js" crossorigin="anonymous"></script>
             </Head>
